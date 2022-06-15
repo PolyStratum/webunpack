7c32c7391c54aefe490d5c3c61edb8be7be5fbca
yellowsink <yellowsink@riseup.net>
2022-06-15 17:46:17 +0100

    moved.

--- src/index.js
+++
@@ -1,2 +0,0 @@
-export * as raid from "./raid/auto";
-export { default as autoraid } from "./raid/auto";
--- src/raid/parcelRequire.js
+++
@@ -1,7 +0,0 @@
-export default () => {
-  const key = Object.keys(window).find((key) =>
-    key.startsWith("parcelRequire")
-  );
-
-  return window[key].cache;
-};
